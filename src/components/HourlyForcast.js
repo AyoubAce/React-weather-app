4dc9f3caa73e131e66e0cd06a80068f5ca17aacb
Ayoub Khribich <36710490+AyoubAce@users.noreply.github.com>
2022-07-12 11:26:50 +0200

    Update HourlyForcast.js

--- src/components/HourlyForcast.js
+++ src/components/HourlyForcast.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext} from "react"
+import React, {useContext} from "react"
 import { WeatherContext } from "./WeatherContext"
 
 export default function HourlyForcast(props){
